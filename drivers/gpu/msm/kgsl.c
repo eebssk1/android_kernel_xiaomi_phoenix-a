c95d90146d909443c257d8de4f4d5611a65606db
EBK21 <chkd13303@gmail.com>
2022-04-25 20:36:45 +0800

    Revert "change kgsl priority"
    This reverts commit 34c40241e62ae7350d2c5209ca757d12e66c0ecc.

--- drivers/gpu/msm/kgsl.c
+++ drivers/gpu/msm/kgsl.c
@@ -5113,7 +5113,7 @@ static void kgsl_core_exit(void)
 static int __init kgsl_core_init(void)
 {
 	int result = 0;
-	struct sched_param param = { .sched_priority = 7 };
+	struct sched_param param = { .sched_priority = 16 };
 
 	place_marker("M - DRIVER KGSL Init");
 
@@ -5193,7 +5193,7 @@ static int __init kgsl_core_init(void)
 		goto err;
 	}
 
-	sched_setscheduler(kgsl_driver.worker_thread, SCHED_RR, &param);
+	sched_setscheduler(kgsl_driver.worker_thread, SCHED_FIFO, &param);
 
 	kgsl_events_init();
 
